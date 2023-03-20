3233961251a8d28aebafe208a48da04ff0caab8b
Jeremy <jeremy.vangansberg@gmail.com>
2023-03-20 12:16:45 +0100

    jinja demo

--- coinplon/main/views.py
+++ coinplon/main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .functions import mutiplicate_by_5
 
-# Create your views here.
+def home_page(request):
+
+    weekdays = [
+        'lundi',
+        'mardi',
+        'mercredi',
+        'jeudi',
+        'vendredi',
+        'samedi',
+        'dimanche'
+    ]
+
+    context = {
+        'test' : mutiplicate_by_5(6),
+        'weekdays' : weekdays
+        }
+    
+    return render(request, 'main/home_page.html', context=context)
