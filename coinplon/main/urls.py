38da656c7da06e0a3eaac12823d6269efb02debe
Jeremy <jeremy.vangansberg@gmail.com>
2023-03-20 14:31:41 +0100

    model - functionnalities

--- coinplon/main/urls.py
+++ coinplon/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home')
+    path('', views.home_page, name='home'),
+    path('about/', views.about, name='about'),
 ]
--- coinplon/main/views.py
+++ coinplon/main/views.py
@@ -2,20 +2,8 @@ from django.shortcuts import render
 from .functions import mutiplicate_by_5
 
 def home_page(request):
+    return render(request, 'main/home_page.html')
 
-    weekdays = [
-        'lundi',
-        'mardi',
-        'mercredi',
-        'jeudi',
-        'vendredi',
-        'samedi',
-        'dimanche'
-    ]
-
-    context = {
-        'test' : mutiplicate_by_5(6),
-        'weekdays' : weekdays
-        }
-    
-    return render(request, 'main/home_page.html', context=context)
+def about(request):
+    # import pudb; pu.db()
+    return render(request, 'main/about.html')
