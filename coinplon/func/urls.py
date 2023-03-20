fd02855d30335b26ab9c34db8a503e5e2e8660f9
Jeremy <jeremy.vangansberg@gmail.com>
2023-03-20 15:11:02 +0100

    class based views exemple

--- coinplon/func/urls.py
+++ coinplon/func/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('func_list/', views.FunctionnalityListView.as_view(), name='func_list')
 ]
--- coinplon/func/views.py
+++ coinplon/func/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Functionnality
+from django.views.generic import ListView
 
-# Create your views here.
+class FunctionnalityListView(ListView):
+    model = Functionnality
+    template_name = "func/func_list.html"
+    context_object_name = "func_list"
