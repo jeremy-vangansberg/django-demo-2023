d3a388e6136cdde857e2a15ea279a11a33259019
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2023-03-21 11:41:22 +0100

    api almost done

--- coinplon/api/views.py
+++ coinplon/api/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from .forms import ApiForm
 from requests import Request, Session
+import os
 
 def api_view(request):
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
     headers = {
         'Accepts': 'application/json',
-        'X-CMC_PRO_API_KEY': 'YOUR_API_KEY'
+        'X-CMC_PRO_API_KEY': os.getenv('API_SECRET_KEY')
     }
+    session = Session()
+    session.headers.update(headers)
+    info = None
 
     form = ApiForm(request.POST)
     if request.method == 'POST':
         if form.is_valid() :
-            print(form.cleaned_data)
-    return render(request, 'api/api_page.html', context={'form': form})
+            response = session.get(url, params=form.cleaned_data)
+            info = response.text
+    return render(request, 'api/api_page.html', context={
+        'form': form,
+        'info': info}
+        )
